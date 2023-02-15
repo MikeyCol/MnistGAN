c212d11badc6a088f1ae5358c970106d10bd0b6d
wash <miacolli@ucsc.edu>
2023-02-14 21:37:52 -0800

    started writing training loop

--- MinstGAN.py
+++ MinstGAN.py
@@ -15,22 +15,19 @@ import numpy as np
 
 device = torch.device("cpu")
 class SamplingLayer(nn.Module):
-	'''
-	Custom layer used to generate a sample from the latent space
-	'''
-    def __init__(self, batch_size,latent_dim,z_mean,z_log_var):
+    '''
+    Custom layer used to generate a sample from the latent space
+    '''
+    def __init__(self, latent_dim):
         super(SamplingLayer, self).__init__()
-        self.batch_size = batch_size
         self.latent_dim = latent_dim
-        self.z_mean = z_mean
-        self.z_log_var = z_log_var
 
-    def forward(self):
-    	'''
-		generates a sample from the latent space
-		'''
-		epsilon = torch.normal(0,1,size=(self.batchSize,self.latent_dim))
-		return self.z_mean + torch.exp(self.z_log_var/2)*epsilon
+    def forward(self,z_mean,z_log_var):
+        '''
+        generates a sample from the latent space
+        '''
+        epsilon = torch.normal(0,1,size=(10,self.latent_dim))
+        return z_mean + torch.exp(z_log_var/2)*epsilon
 
 
 
@@ -42,77 +39,118 @@ class Encoder(nn.Module):
       self.l_relu = nn.LeakyReLU()
       self.z_mean = nn.Linear(intermediate_dim, latent_dim)
       self.z_log_var = nn.Linear(intermediate_dim, latent_dim)
-      self.z = SamplingLayer(self.z_mean,self.z_log_var)
+      self.z = SamplingLayer(latent_dim)
 
     def forward(self,x):
-    	'''
-    	'''
+        '''
+        '''
 
-    	x = self.fc1(x)
-    	x = self.l_relu(x)
-    	z_mean = self.z_mean(x)
-    	z_long_var = self.z_log_var(x)
-    	return self.z(z_mean,z_log_var)
+        x = self.fc1(x)
+        x = self.l_relu(x)
+        z_mean = self.z_mean(x)
+        z_log_var = self.z_log_var(x)
+        return self.z.forward(z_mean,z_log_var)
 
     
 class Decoder(nn.Module):
-	def __init__(self,original_dim,latent_dim,intermediate_dim):
-		self.fc1 = nn.Linear(latent_dim,intermediate_dim)
-		self.fc2 = nn.Linear(intermediate_dim,original_dim)
-		self.l_relu = nn.LeakyReLU()
-		self.sigmoid = nn.Sigmoid()
+    def __init__(self,original_dim,latent_dim,intermediate_dim):
+        super(Decoder, self).__init__()
+        self.fc1 = nn.Linear(latent_dim,intermediate_dim)
+        self.fc2 = nn.Linear(intermediate_dim,original_dim)
+        self.l_relu = nn.LeakyReLU()
+        self.sigmoid = nn.Sigmoid()
 
-	def forward(self,x)
-		x = self.fc1(x)
-		x = self.l_relu(x)
-		x = self.fc2(x)
-		return self.sigmoid(x)
-		
+    def forward(self,x):
+        x = self.fc1(x)
+        x = self.l_relu(x)
+        x = self.fc2(x)
+        return self.sigmoid(x)
+        
 class AutoEncoder(nn.Module):
-	def __init__(self,original_dim,latent_dim,intermediate_dim):
-		self.Encoder(original_dim,latent_dim,intermediate_dim):
-		self.Decoder(original_dim,latent_dim,intermediate_dim):
+    def __init__(self,original_dim,latent_dim,intermediate_dim):
+        super(AutoEncoder, self).__init__()
+        self.Encoder = Encoder(original_dim,latent_dim,intermediate_dim)
+        self.Decoder = Decoder(original_dim,latent_dim,intermediate_dim)
 
-	def forward(self,x):
-		'''
-		'''
-		z = self.Encoder(x)
-		return self.Decoder(z)
+    def forward(self,x):
+        '''
+        '''
+        x = self.Encoder.forward(x)
+        print(x)
+        print(x.shape)
+        z_mean = x[:,0]
+        print(z_mean)
+        z_log_var = x[:,-1]
+        print(z_log_var)
+        x_decoded_mean = np.empty(x.shape[0])
+        for i in range(x.shape[0]):
+            print(x[i][0])
+            print(x[i][1])
+            print(x[i])
+            print(x[i].shape)
+            x_decoded_mean[i] = self.Decoder.forward(x[i].clone())
 
-class VaeLoss(nn.Module):
-	'''
-	'''
-	def __init__(self):
-		super(VaeLoss,self).__init__()
-		self.BCELoss = nn.BCELoss()
+        return torch.from_numpy(x_decoded_mean), torch.from_numpy(z_mean), torch.from_numpy(z_log_var)
 
-	def forward(self,x,x_decoded_mean,z_log_var,z_mean,original_dim=28*28):
-		xent_loss = original_dim*self.BCELoss(x,x_decoded_mean)
-		kl_loss = -0.5*torch.sum(1 + z_log_var - torch.square(z_mean) - torch.exp(z_log_var))
-		return xent_loss + kl_loss
+def vaeLoss(x,x_decoded_mean,z_mean,z_log_var,original_dim=28*28):
+    '''
+    '''
+    BCELoss = nn.BCELoss()
+
+    xent_loss = original_dim*BCELoss(x_decoded_mean,x)
+    kl_loss = -0.5*torch.sum(1 + z_log_var - torch.square(z_mean) - torch.exp(z_log_var))
+    return xent_loss + kl_loss
 
 def main():
 
-	(train_X, train_y), (test_X, test_y) = mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-	print('X_train: ' + str(train_X.shape))
-	print('Y_train: ' + str(train_y.shape))
-	print('X_test:  '  + str(test_X.shape))
-	print('Y_test:  '  + str(test_y.shape))
+    print('X_train: ' + str(x_train.shape))
+    print('Y_train: ' + str(y_train.shape))
+    print('X_test:  '  + str(x_test.shape))
+    print('Y_test:  '  + str(y_test.shape))
 
-	x_train = x_train.astype('float32') / 255.
-	x_test = x_test.astype('float32') / 255.
-	x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-	x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+    x_train = x_train.astype('float32') / 255.
+    x_test = x_test.astype('float32') / 255.
+    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
-	batchSize = 100
-	original_dim = 28*28
-	latent_dim = 2
-	intermediate_dim = 256
-	epochs = 5
-	epsilonStd = 1.0
 
-	for epoch in range(epochs):
+    print('X_train: ' + str(x_train.shape))
+    print('Y_train: ' + str(y_train.shape))
+    print('X_test:  '  + str(x_test.shape))
+    print('Y_test:  '  + str(y_test.shape))
+
+    batchSize = 10
+    original_dim = 28*28
+    latent_dim = 2
+    intermediate_dim = 256
+    epochs = 5
+    epsilonStd = 1.0
+
+    auto_model = AutoEncoder(original_dim,latent_dim,intermediate_dim)
+
+    for name,l in auto_model.named_children():
+        print(f"{name}:{l}")
+
+    for epoch in range(epochs):
+        for i in range(x_train.shape[0]):
+            x_decoded_mean, z_mean, z_log_var = auto_model.forward(torch.from_numpy(x_train[i]))
+            print('###################################################################')
+            i_train = torch.tensor([x_train[i] for j in range(batchSize)])
+            print('i_train \n',i_train)
+            print(i_train.shape)
+            print('x_decoded_mean \n',x_decoded_mean)
+            print(x_decoded_mean.shape)
+            print('z_mean \n',z_mean)
+            print(z_mean.shape)
+            print('z_log_var \n',z_log_var)
+            print(z_log_var.shape)
+            
+            loss = vaeLoss(i_train,x_decoded_mean,z_mean,z_log_var)
+            print(loss)
+            loss /= batchSize
+            print('loss: ',loss)
 
 if __name__ == "__main__":
     main()  
